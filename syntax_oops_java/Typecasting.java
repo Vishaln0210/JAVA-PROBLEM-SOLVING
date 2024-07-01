69b76ec11cc36ee04ddd23db8c121e323bf52111
Vishaln_2004 <vishalitisthatis2004@gmail.com>
2024-07-01 23:07:07 +0530

    commit

--- syntax_oops_java/Typecasting.java
+++ syntax_oops_java/Typecasting.java
@@ -1,4 +1,14 @@
 package syntax_oops_java;
 
+import java.util.Scanner;
+
 public class Typecasting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = (int)67.45;
+        System.out.println(num);
+        int a = 223;
+        System.out.println((byte)a);
+    }
+
 }
